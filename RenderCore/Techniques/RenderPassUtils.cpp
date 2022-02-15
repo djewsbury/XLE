80d9e7922e8b8a1c6b29329e2f06f6b60b71fff7
David Jewsbury <djewsbury@gmail.com>
2022-02-15 11:44:51 +1000

    Added the ability to specify a clear color in RenderPassUtils - not that the clear color will be used directly, with no SRGB/Linear conversions

--- RenderCore/Techniques/RenderPassUtils.cpp
+++ RenderCore/Techniques/RenderPassUtils.cpp
@@ -13,33 +13,47 @@
 
 namespace RenderCore { namespace Techniques
 {
+	static ClearValue AsClearValueColor(unsigned clearColor) 
+	{
+		ClearValue clear;
+		clear._float[0] = ((clearColor>>16)&0xff)/float(0xff);
+		clear._float[1] = ((clearColor>> 8)&0xff)/float(0xff);
+		clear._float[2] = ((clearColor>> 0)&0xff)/float(0xff);
+		clear._float[3] = ((clearColor>>24)&0xff)/float(0xff);
+		return clear;
+	}
+
 	RenderPassInstance RenderPassToPresentationTarget(
         ParsingContext& parserContext,
-		LoadStore loadOperation)
+		LoadStore loadOperation,
+		unsigned clearColor)
 	{
 		FrameBufferDescFragment frag;
 		SubpassDesc subpass;
 		subpass.AppendOutput(frag.DefineAttachment(AttachmentSemantics::ColorLDR).InitialState(loadOperation, 0));
 		frag.AddSubpass(std::move(subpass));
-        return RenderPassInstance{parserContext, frag};
+		auto clear = AsClearValueColor(clearColor);
+        return RenderPassInstance{parserContext, frag, RenderPassBeginDesc{MakeIteratorRange(&clear, &clear+1)}};
 	}
 
 	RenderPassInstance RenderPassToPresentationTarget(
 		const RenderCore::IResourcePtr& presentationTarget,
         ParsingContext& parserContext,
-		LoadStore loadOperation)
+		LoadStore loadOperation,
+		unsigned clearColor)
 	{
 		parserContext.GetTechniqueContext()._attachmentPool->Bind(AttachmentSemantics::ColorLDR, presentationTarget);
-		return RenderPassToPresentationTarget(parserContext, loadOperation);
+		return RenderPassToPresentationTarget(parserContext, loadOperation, clearColor);
 	}
 
 	RenderPassInstance RenderPassToPresentationTargetWithDepthStencil(
         ParsingContext& parserContext,
-		LoadStore loadOperation)
+		LoadStore loadOperation,
+		unsigned clearColor)
 	{
 		auto boundDepth = parserContext.GetTechniqueContext()._attachmentPool->GetBoundResource(AttachmentSemantics::MultisampleDepth);
 		if (!boundDepth && loadOperation != LoadStore::Clear)
-			return RenderPassToPresentationTarget(parserContext, loadOperation);
+			return RenderPassToPresentationTarget(parserContext, loadOperation, clearColor);
 
 		FrameBufferDescFragment frag;
 		SubpassDesc subpass;
@@ -47,15 +61,17 @@ namespace RenderCore { namespace Techniques
 		subpass.SetDepthStencil(frag.DefineAttachment(AttachmentSemantics::MultisampleDepth).InitialState(loadOperation, 0));
 		frag.AddSubpass(std::move(subpass));
 
-        return RenderPassInstance{ parserContext, frag };
+        auto clear = AsClearValueColor(clearColor);
+		return RenderPassInstance{ parserContext, frag, RenderPassBeginDesc{MakeIteratorRange(&clear, &clear+1)}};
 	}
 
 	RenderPassInstance RenderPassToPresentationTargetWithDepthStencil(
 		const std::shared_ptr<RenderCore::IResource>& presentationTarget,
         ParsingContext& parserContext,
-		LoadStore loadOperation)
+		LoadStore loadOperation,
+		unsigned clearColor)
 	{
         parserContext.GetTechniqueContext()._attachmentPool->Bind(AttachmentSemantics::ColorLDR, presentationTarget);
-		return RenderPassToPresentationTargetWithDepthStencil(parserContext, loadOperation);
+		return RenderPassToPresentationTargetWithDepthStencil(parserContext, loadOperation, clearColor);
 	}
 }}
