a3747370653b7fcf7979de88d7ef0aee4c514bea
David Jewsbury <djewsbury@gmail.com>
2023-01-24 13:01:19 +1000

    Separated out the "history confidence" calculations from depth-plus-util - still unclear if we always want to calculate this in the depth plus shader, or in a dedicated pass, or just on demand as required

---
+++ Working/Game/xleres/TechniqueLibrary/LightingEngine/history-confidence.hlsl
@@ -0,0 +1,51 @@
+// Distributed under the MIT License (See
+// accompanying file "LICENSE" or the website
+// http://www.opensource.org/licenses/mit-license.php)
+
+// Based on approach from AMD SSR shaders
+float CalculateEdgeStoppingNormalWeight(float3 normalToday, float3 normalYesterday)
+{
+	const float sigma = 4.0;
+	return pow(max(dot(normalToday, normalYesterday), 0.0), sigma);
+}
+
+float CalculateEdgeStoppingRoughnessWeight(float roughnessToday, float roughnessYesterday)
+{
+	// often we're writing out roughness as a 256 bit value -- so we can only be so accurate
+	const float sigmaMin = 2.f / 256.f;
+	const float sigmaMax = 12.f / 256.f;
+	return 1.0 - smoothstep(sigmaMin, sigmaMax, abs(roughnessToday - roughnessYesterday));
+}
+
+float CalculateEdgeStoppingDepthWeight(float depthToday, float depthYesterday)
+{
+	float ratio = min(depthToday, depthYesterday) / max(depthToday, depthYesterday);
+	return smoothstep(1-128.f/65535.f, 1-32.f/65535.f, ratio);
+}
+
+float4 LoadNormalAndRoughnessPrev(uint2 loadPos);
+float LoadDepthPrev(uint2 loadPos);
+
+float CalculatePixelHistoryConfidence(
+	uint2 todayCoord, int2 motion,
+	float3 todayWorldSpaceNormal, float todayRoughness, float todayDepth,
+	uint2 depthBufferDims)
+{
+	int2 loadPos = int2(todayCoord.xy+motion.xy);
+	// Note that we consider the texture size here, not the viewport
+	if (any(loadPos < 0 || loadPos >= depthBufferDims))
+		return 0;
+
+	float yesterdayDepth = LoadDepthPrev(loadPos);
+	if (yesterdayDepth == 0) 
+		return 0;
+
+	float4 yesterdayNormalAndRoughness = LoadNormalAndRoughnessPrev(loadPos);
+	
+	// Note that resulting confidence will always be reduced a little bit just because of the normal compression method
+	return
+		  CalculateEdgeStoppingNormalWeight(todayWorldSpaceNormal, yesterdayNormalAndRoughness.xyz)
+		* CalculateEdgeStoppingRoughnessWeight(todayRoughness, yesterdayNormalAndRoughness.w)
+		* CalculateEdgeStoppingDepthWeight(todayDepth, yesterdayDepth)
+		;
+}
--- Working/Game/xleres/TechniqueLibrary/Standard/depth-plus-util.hlsl
+++ Working/Game/xleres/TechniqueLibrary/Standard/depth-plus-util.hlsl
@@ -4,6 +4,7 @@
 
 #include "../Framework/gbuffer.hlsl"
 #include "../Framework/Binding.hlsl"
+#include "../LightingEngine/history-confidence.hlsl"
 
 #if !defined(VSOUT_H)
 	#error Include VSOUT.hlsl before including this file
@@ -16,28 +17,20 @@
 #if DEPTH_PLUS_HISTORY_ACCUMULATION
 	Texture2D<float> DepthPrev BIND_SEQ_T6;
 	Texture2D<float4> GBufferNormalPrev BIND_SEQ_T7;
+
+	float4 LoadNormalAndRoughnessPrev(uint2 loadPos)
+	{
+		float4 yesterdayNormalSample = GBufferNormalPrev.Load(int3(loadPos, 0));
+		float3 yesterdayNormal = DecodeGBufferNormal(yesterdayNormalSample.xyz);
+		float yesterdayRoughness = yesterdayNormalSample.w;
+		return float4(yesterdayNormal, yesterdayRoughness);
+	}
+
+	float LoadDepthPrev(uint2 loadPos)
+	{
+		return DepthPrev.Load(int3(loadPos, 0));
+	}
 #endif
-
-// Based on approach from AMD SSR shaders
-float CalculateEdgeStoppingNormalWeight(float3 normalToday, float3 normalYesterday)
-{
-	const float sigma = 4.0;
-	return pow(max(dot(normalToday, normalYesterday), 0.0), sigma);
-}
-
-float CalculateEdgeStoppingRoughnessWeight(float roughnessToday, float roughnessYesterday)
-{
-	const float sigmaMin = 0.001f;
-	const float sigmaMax = 0.01f;
-	return 1.0 - smoothstep(sigmaMin, sigmaMax, abs(roughnessToday - roughnessYesterday));
-}
-
-float CalculateEdgeStoppingDepthWeight(float depthToday, float depthYesterday)
-{
-	float ratio = min(depthToday, depthYesterday) / max(depthToday, depthYesterday);
-	if (ratio > 0.9) return smoothstep(0, 1, (ratio-0.9)/0.1);
-	return 0;
-}
 
 DepthPlusEncoded ResolveDepthPlus(VSOUT geo, GBufferValues sample)
 {
@@ -50,26 +43,12 @@ DepthPlusEncoded ResolveDepthPlus(VSOUT geo, GBufferValues sample)
 		prevPos.xy = clamp(round(prevPos.xy), -127, 127);
 
 		#if DEPTH_PLUS_HISTORY_ACCUMULATION
-			int2 loadPos = int2(geo.position.xy+prevPos.xy);
-			// Note that we consider the texture size here, not the viewport
 			int2 depthPrevDims;
 			DepthPrev.GetDimensions(depthPrevDims.x, depthPrevDims.y);
-			if (all(loadPos > 0 && loadPos < depthPrevDims)) {
-				float depthPrev = DepthPrev.Load(int3(loadPos, 0));
-				float4 normalSamplePrev = GBufferNormalPrev.Load(int3(loadPos, 0));
-				float3 normalPrev = DecodeGBufferNormal(normalSamplePrev.xyz);
-				float roughnessPrev = normalSamplePrev.w;
-				// Note that historyAccumulationWeight will be reduced a little bit just because of the normal compression method
-				historyAccumulationWeight
-					= CalculateEdgeStoppingNormalWeight(sample.worldSpaceNormal, normalPrev)
-					* CalculateEdgeStoppingRoughnessWeight(sample.material.roughness, roughnessPrev)
-					* CalculateEdgeStoppingDepthWeight(geo.prevPosition.z / geo.prevPosition.w, depthPrev)
-					;
-
-				if (depthPrev == 0) historyAccumulationWeight = 0;
-			} else {
-				historyAccumulationWeight = 0;
-			}
+			historyAccumulationWeight = CalculatePixelHistoryConfidence(
+				geo.position.xy, prevPos.xy,
+				sample.worldSpaceNormal, sample.material.roughness, geo.prevPosition.z / geo.prevPosition.w,
+				depthPrevDims);
 		#endif
 	#else
 		prevPos = 0.0.xxx;
