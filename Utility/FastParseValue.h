93eeff70fb27a691d2ed347e43207a03664ac593
David Jewsbury <djewsbury@gmail.com>
2023-04-11 13:44:25 +1000

    FastParseValues functions & HashCombine marked as "constexpr"

--- Utility/FastParseValue.h
+++ Utility/FastParseValue.h
@@ -16,16 +16,204 @@ namespace Utility
         So the implementation here isn't actually 100%... but it works in at least some cases, and
         is quick. Just be careful with it -- because it's not guaranteed to be correct
     */
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst, unsigned radix);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst, unsigned radix);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst, unsigned radix);
-    template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst, unsigned radix);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst, unsigned radix);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst, unsigned radix);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst, unsigned radix);
+    template<typename CharType> constexpr const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst, unsigned radix);
     template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, float& dst);
     template<typename CharType> const CharType* FastParseValue(StringSection<CharType> input, double& dst);
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst)
+    {
+        bool positive = true;
+        auto iterator = input.begin();
+
+        if (iterator >= input.end()) return iterator;
+        if (*iterator == '-') { positive = false; ++iterator; }
+        else if (*iterator == '+') ++iterator;
+
+        uint32_t result = 0;
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator < '0' || *iterator > '9') break;
+
+            result = (result * 10) + uint32_t((*iterator) - '0');
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = positive ? result : -int32_t(result);
+        return iterator;
+    }
+    
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst)
+    {
+        bool positive = true;
+        auto iterator = input.begin();
+
+        if (iterator >= input.end()) return iterator;
+        if (*iterator == '-') { positive = false; ++iterator; }
+        else if (*iterator == '+') ++iterator;
+
+        uint64_t result = 0;
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator < '0' || *iterator > '9') break;
+
+            result = (result * 10ull) + uint64_t((*iterator) - '0');
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = positive ? result : -int64_t(result);
+        return iterator;
+    }
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst)
+    {
+        uint64_t result = 0;
+        auto iterator = input.begin();
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator < '0' || *iterator > '9') break;
+
+            result = (result * 10ull) + uint64_t((*iterator) - '0');
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = result;
+        return iterator;
+    }
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst)
+    {
+        uint32_t result = 0;
+        auto iterator = input.begin();
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator < '0' || *iterator > '9') break;
+
+            result = (result * 10u) + uint32_t((*iterator) - '0');
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = result;
+        return iterator;
+    }
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, int32_t& dst, unsigned radix)
+    {
+        bool positive = true;
+        auto iterator = input.begin();
+
+        if (iterator >= input.end()) return iterator;
+        if (*iterator == '-') { positive = false; ++iterator; }
+        else if (*iterator == '+') ++iterator;
+
+        uint32_t result = 0;
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator >= '0' && *iterator <= '9') {
+                if (((*iterator) - '0') >= CharType(radix)) break;
+                result = (result * radix) + uint32_t((*iterator) - '0');
+            } else if (*iterator >= 'a' && *iterator <= CharType('a'+radix-11)) {
+                result = (result * radix) + uint32_t((*iterator) - 'a' + 10);
+            } else if (*iterator >= 'A' && *iterator <= CharType('A'+radix-11)) {
+                result = (result * radix) + uint32_t((*iterator) - 'A' + 10);
+            } else
+                break;
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = positive ? result : -int32_t(result);
+        return iterator;
+    }
+    
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, int64_t& dst, unsigned radix)
+    {
+        bool positive = true;
+        auto iterator = input.begin();
+
+        if (iterator >= input.end()) return iterator;
+        if (*iterator == '-') { positive = false; ++iterator; }
+        else if (*iterator == '+') ++iterator;
+
+        uint64_t result = 0;
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator >= '0' && *iterator <= '9') {
+                if (((*iterator) - '0') >= CharType(radix)) break;
+                result = (result * radix) + uint64_t((*iterator) - '0');
+            } else if (*iterator >= 'a' && *iterator <= CharType('a'+radix-11)) {
+                result = (result * radix) + uint64_t((*iterator) - 'a' + 10);
+            } else if (*iterator >= 'A' && *iterator <= CharType('A'+radix-11)) {
+                result = (result * radix) + uint64_t((*iterator) - 'A' + 10);
+            } else
+                break;
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = positive ? result : -int64_t(result);
+        return iterator;
+    }
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, uint64_t& dst, unsigned radix)
+    {
+        uint64_t result = 0;
+        auto iterator = input.begin();
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator >= '0' && *iterator <= '9') {
+                if (((*iterator) - '0') >= CharType(radix)) break;
+                result = (result * radix) + uint64_t((*iterator) - '0');
+            } else if (*iterator >= 'a' && *iterator <= CharType('a'+radix-11)) {
+                result = (result * radix) + uint64_t((*iterator) - 'a' + 10);
+            } else if (*iterator >= 'A' && *iterator <= CharType('A'+radix-11)) {
+                result = (result * radix) + uint64_t((*iterator) - 'A' + 10);
+            } else
+                break;
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = result;
+        return iterator;
+    }
+
+    template<typename CharType>
+        constexpr const CharType* FastParseValue(StringSection<CharType> input, uint32_t& dst, unsigned radix)
+    {
+        uint32_t result = 0;
+        auto iterator = input.begin();
+        for (;;) {
+            if (iterator >= input.end()) break;
+            if (*iterator >= '0' && *iterator <= '9') {
+                if (((*iterator) - '0') >= CharType(radix)) break;
+                result = (result * radix) + uint32_t((*iterator) - '0');
+            } else if (*iterator >= 'a' && *iterator <= CharType('a'+radix-11)) {
+                result = (result * radix) + uint32_t((*iterator) - 'a' + 10);
+            } else if (*iterator >= 'A' && *iterator <= CharType('A'+radix-11)) {
+                result = (result * radix) + uint32_t((*iterator) - 'A' + 10);
+            } else
+                break;
+            ++iterator;
+        }
+        if (iterator != input.begin())
+            dst = result;
+        return iterator;
+    }
+
 }
 
 using namespace Utility;
