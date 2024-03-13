4111ef39b380791c0ab3cb3b176252b426564663
David Jewsbury <djewsbury@gmail.com>
2024-03-13 12:48:32 +1000

    VMA will now query vulkan functions from the dll - this avoids statically linking some vulkan functions, which can create version incompatibility issues when run on machines with very old vulkan installs

--- RenderCore/Vulkan/Metal/VMAImplementation.cpp
+++ RenderCore/Vulkan/Metal/VMAImplementation.cpp
@@ -3,4 +3,6 @@
 // http://www.opensource.org/licenses/mit-license.php)
 
 #define VMA_IMPLEMENTATION		// function implementations get compiled in this translation unit
+#define VMA_STATIC_VULKAN_FUNCTIONS 0
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
 #include "Foreign/VulkanMemoryAllocator/include/vk_mem_alloc.h"
