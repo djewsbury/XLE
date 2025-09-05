3ac25291f9922a7c08c5b1027d9fe261c83861e2
David Jewsbury <djewsbury@gmail.com>
2025-09-05 12:54:02 +1000

    Support for joint indices greater than 255 in the skin deform shader

--- xleres/core/Deform/skin.compute.hlsl
+++ xleres/core/Deform/skin.compute.hlsl
@@ -2,7 +2,7 @@
 #include "deform-helper.compute.hlsl"
 
 #if INFLUENCE_COUNT > 0
-	#if JOINT_INDICES_TYPE != 2 || JOINT_INDICES_PRECISION != 8
+	#if JOINT_INDICES_TYPE != 2 || (JOINT_INDICES_PRECISION != 8 && JOINT_INDICES_PRECISION != 16)
 		#error Unsupported skinning joint indicies type
 	#endif
 
@@ -25,19 +25,19 @@ StructuredBuffer<SkinIAParamsStruct> SkinIAParams : register(t6);
 #define row_major_float3x4 row_major float3x4
 StructuredBuffer<row_major_float3x4> JointTransforms : register(t7);
 
-uint LoadWeightPack(uint vertexIdx, uint influenceCount, SkinIAParamsStruct iaParams)
+uint LoadWeightPack(uint vertexIdx, uint influenceByteOffset, SkinIAParamsStruct iaParams)
 {
 	// Alignment rules (also applies to LoadIndexPack)
 	// 1 influence/vertex: any alignment ok
 	// 2 influences/vertex: must be aligned to multiple of 2
 	// 4 or more influences/vertex: must be aligned to multiple of 4
-	uint offset = iaParams.WeightsOffset+vertexIdx*iaParams.StaticVertexAttachmentsStride+influenceCount;
+	uint offset = iaParams.WeightsOffset+vertexIdx*iaParams.StaticVertexAttachmentsStride+influenceByteOffset;
 	return StaticVertexAttachments.Load(offset&(~3)) >> ((offset&3)*8);
 }
 
-uint LoadIndexPack(uint vertexIdx, uint influenceCount, SkinIAParamsStruct iaParams)
+uint LoadIndexPack(uint vertexIdx, uint influenceByteOffset, SkinIAParamsStruct iaParams)
 {
-	uint offset = iaParams.JointIndicesOffsets+vertexIdx*iaParams.StaticVertexAttachmentsStride+influenceCount;
+	uint offset = iaParams.JointIndicesOffsets+vertexIdx*iaParams.StaticVertexAttachmentsStride+influenceByteOffset;
 	return StaticVertexAttachments.Load(offset&(~3)) >> ((offset&3)*8);
 }
 
@@ -67,18 +67,23 @@ DeformVertex PerformDeform(DeformVertex input, uint vertexIdx, uint instanceIdx)
 	float3 outputPosition = 0.0.xxx;
 	float3 outputNormal = 0.0.xxx;
 	float3 outputTangent = 0.0.xxx;
+	const float reciprocal255 = 1.0f / 255.f;
 
-	#if INFLUENCE_COUNT > 0
+	#if INFLUENCE_COUNT > 0 && JOINT_INDICES_PRECISION == 8
+
+		// 8 bit joint indices (weights also 8 bit packed)
+		// we process 4 at a time so we can load in 32 bit integers from the vertex buffer
+
 		uint c=0;
 		for (;;) {
 			uint packedWeights = LoadWeightPack(vertexIdx, c, skinIAParams);
-			uint packedIndices = LoadIndexPack(vertexIdx, c, skinIAParams);
+			uint packedIndices = LoadIndexPackUINT8(vertexIdx, c, skinIAParams);
 
 			{
 				uint boneIndex = packedIndices&0xff;
 				boneIndex += firstJointTransform;
 				packedIndices >>= 8;
-				float weight = (packedWeights&0xff) / 255.f;
+				float weight = (packedWeights&0xff) * reciprocal255;
 				packedWeights >>= 8;
 
 				outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
@@ -95,7 +100,7 @@ DeformVertex PerformDeform(DeformVertex input, uint vertexIdx, uint instanceIdx)
 					uint boneIndex = packedIndices&0xff;
 					boneIndex += firstJointTransform;
 					packedIndices >>= 8;
-					float weight = (packedWeights&0xff) / 255.f;
+					float weight = (packedWeights&0xff) * reciprocal255;
 					packedWeights >>= 8;
 
 					outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
@@ -112,7 +117,7 @@ DeformVertex PerformDeform(DeformVertex input, uint vertexIdx, uint instanceIdx)
 						uint boneIndex = packedIndices&0xff;
 						boneIndex += firstJointTransform;
 						packedIndices >>= 8;
-						float weight = (packedWeights&0xff) / 255.f;
+						float weight = (packedWeights&0xff) * reciprocal255;
 						packedWeights >>= 8;
 
 						outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
@@ -127,9 +132,7 @@ DeformVertex PerformDeform(DeformVertex input, uint vertexIdx, uint instanceIdx)
 					{
 						uint boneIndex = packedIndices&0xff;
 						boneIndex += firstJointTransform;
-						packedIndices >>= 8;
-						float weight = (packedWeights&0xff) / 255.f;
-						packedWeights >>= 8;
+						float weight = (packedWeights&0xff) * reciprocal255;
 
 						outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
 						float3x3 rotationPart = float3x3(JointTransforms[boneIndex][0].xyz, JointTransforms[boneIndex][1].xyz, JointTransforms[boneIndex][2].xyz);
@@ -142,6 +145,84 @@ DeformVertex PerformDeform(DeformVertex input, uint vertexIdx, uint instanceIdx)
 				#endif
 			#endif
 		}
+
+	#elif INFLUENCE_COUNT > 0 && JOINT_INDICES_PRECISION == 16
+
+		// 16 bit joint indices (weights also 8 bit packed)
+		// we still process 4 at a time, because of the weights are still 8 bit packed
+
+		uint c=0;
+		for (;;) {
+			uint packedWeights = LoadWeightPack(vertexIdx, c, skinIAParams);
+			uint packedIndices0 = LoadIndexPack(vertexIdx, c*2, skinIAParams);
+			uint packedIndices1 = LoadIndexPack(vertexIdx, c*2+4, skinIAParams);
+
+			{
+				uint boneIndex = packedIndices0&0xffff;
+				boneIndex += firstJointTransform;
+				packedIndices0 >>= 16;
+				float weight = (packedWeights&0xff) * reciprocal255;
+				packedWeights >>= 8;
+
+				outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
+				float3x3 rotationPart = float3x3(JointTransforms[boneIndex][0].xyz, JointTransforms[boneIndex][1].xyz, JointTransforms[boneIndex][2].xyz);
+				outputNormal += weight * mul(rotationPart, input.normal.xyz);
+				outputTangent += weight * mul(rotationPart, input.tangent.xyz);
+
+				++c;
+				if (c == skinInvocationParams.SoftInfluenceCount) break;
+			}
+
+			#if INFLUENCE_COUNT > 1
+				{
+					uint boneIndex = packedIndices0&0xffff;
+					boneIndex += firstJointTransform;
+					float weight = (packedWeights&0xff) * reciprocal255;
+					packedWeights >>= 8;
+
+					outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
+					float3x3 rotationPart = float3x3(JointTransforms[boneIndex][0].xyz, JointTransforms[boneIndex][1].xyz, JointTransforms[boneIndex][2].xyz);
+					outputNormal += weight * mul(rotationPart, input.normal.xyz);
+					outputTangent += weight * mul(rotationPart, input.tangent.xyz);
+
+					++c;
+					if (c == skinInvocationParams.SoftInfluenceCount) break;
+				}
+
+				#if INFLUENCE_COUNT > 2
+					{
+						uint boneIndex = packedIndices1&0xffff;
+						boneIndex += firstJointTransform;
+						packedIndices1 >>= 16;
+						float weight = (packedWeights&0xff) * reciprocal255;
+						packedWeights >>= 8;
+
+						outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
+						float3x3 rotationPart = float3x3(JointTransforms[boneIndex][0].xyz, JointTransforms[boneIndex][1].xyz, JointTransforms[boneIndex][2].xyz);
+						outputNormal += weight * mul(rotationPart, input.normal.xyz);
+						outputTangent += weight * mul(rotationPart, input.tangent.xyz);
+
+						++c;
+						if (c == skinInvocationParams.SoftInfluenceCount) break;
+					}
+
+					{
+						uint boneIndex = packedIndices1&0xffff;
+						boneIndex += firstJointTransform;
+						float weight = (packedWeights&0xff) * reciprocal255;
+
+						outputPosition += weight * mul(JointTransforms[boneIndex], float4(input.position.xyz, 1)).xyz;
+						float3x3 rotationPart = float3x3(JointTransforms[boneIndex][0].xyz, JointTransforms[boneIndex][1].xyz, JointTransforms[boneIndex][2].xyz);
+						outputNormal += weight * mul(rotationPart, input.normal.xyz);
+						outputTangent += weight * mul(rotationPart, input.tangent.xyz);
+
+						++c;
+						if (c == skinInvocationParams.SoftInfluenceCount) break;
+					}
+				#endif
+			#endif
+		}
+
 	#else
 		outputPosition = input.position;
 		outputNormal = input.normal.xyz;
