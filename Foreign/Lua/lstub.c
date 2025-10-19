4e45f7255e98e1c3b11d115ad314fef8ca599478
David Jewsbury <djewsbury@gmail.com>
2025-10-20 09:48:09 +1000

    Added the lula coroutine library back into the link

--- Foreign/Lua/lstub.c
+++ Foreign/Lua/lstub.c
@@ -9,11 +9,6 @@ LUAMOD_API int luaopen_package (lua_State *L)
 	return 0;
 }
 
-LUAMOD_API int luaopen_coroutine (lua_State *L)
-{
-	return 0;
-}
-
 LUAMOD_API int luaopen_io (lua_State *L)
 {
 	return 0;
